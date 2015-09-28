82ab8f6bbf0ee9d68e6e1ff73ad4e2931f247b6f
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-09-28 11:01:40 +0100

    Adding school context

--- EF-Code-First-Tutorials/EF-Code-First-Tutorials/Standard.cs
+++ EF-Code-First-Tutorials/EF-Code-First-Tutorials/Standard.cs
@@ -8,6 +8,13 @@ namespace EF_Code_First_Tutorials
 {
     public class Standard
     {
+        public Standard()
+        {
 
+        }
+        public int StandardID { get; set; }
+        public string StandardName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
     }
 }
