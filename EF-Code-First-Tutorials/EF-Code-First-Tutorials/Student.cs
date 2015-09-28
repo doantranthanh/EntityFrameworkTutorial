43a356a4b98879a18338f1d94bbcdbf463d1047f
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-09-28 10:34:17 +0100

    Adding Student Domain Class

--- EF-Code-First-Tutorials/EF-Code-First-Tutorials/Student.cs
+++ EF-Code-First-Tutorials/EF-Code-First-Tutorials/Student.cs
@@ -8,5 +8,18 @@ namespace EF_Code_First_Tutorials
 {
     public class Student
     {
+        public Student()
+        {
+
+        }
+
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Standard Standard { get; set; }
     }
 }
